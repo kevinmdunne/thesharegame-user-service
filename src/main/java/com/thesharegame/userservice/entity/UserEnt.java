7cfa90835c7e0078beb10af38887788781793c2c
Kevin Dunne <kevinmdunne@users.noreply.github.com>
2024-03-07 22:28:59 +0000

    add liquibase

---
+++ src/main/java/com/thesharegame/userservice/entity/UserEnt.java
@@ -0,0 +1,9 @@
+package com.thesharegame.userservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class UserEnt {
+}
---
+++ src/main/resources/db/changelog/changes/v1.sql
@@ -0,0 +1,6 @@
+create table "user" (
+  id varchar(255) not null,
+  name varchar(50) not null,
+  primary key (id)
+);
+
