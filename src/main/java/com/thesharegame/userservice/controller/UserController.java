7c29b4aa72edd9dc6d25a24c4a3593a9139576f9
Kevin Dunne <kevinmdunne@users.noreply.github.com>
2024-10-07 22:26:38 +0100

    add logging and docker arguments

--- src/main/java/com/thesharegame/userservice/controller/UserController.java
+++ src/main/java/com/thesharegame/userservice/controller/UserController.java
@@ -2,10 +2,12 @@ package com.thesharegame.userservice.controller;
 
 import com.thesharegame.userservice.dto.UserDto;
 import com.thesharegame.userservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
+@Slf4j
 public class UserController {
 
     private UserService userService;
@@ -16,21 +18,25 @@ public class UserController {
 
     @GetMapping("/getbyid/{id}")
     public UserDto getUserById(@PathVariable String id){
+        log.info("Calling getUserById");
         return userService.getUserById(id);
     }
 
     @PostMapping("/saveuser")
     public UserDto saveUser(@RequestBody UserDto userDto){
+        log.info("Calling saveUser");
         return userService.saveUser(userDto);
     }
 
     @DeleteMapping("/deleteuser/{id}")
     public void deleteUser(@PathVariable String id){
+        log.info("Calling deleteUser");
         userService.deleteUser(id);
     }
 
     @GetMapping("/addDummyData")
     public UserDto addDummyData(){
+        log.info("Calling addDummyData");
         return this.userService.addDummyData();
     }
 }
